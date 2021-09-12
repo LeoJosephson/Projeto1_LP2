96425bfdf68a0a651d1bca3f8e54e99f6c4cb345
LeoJosephson <leonardojosephson24@gmail.com>
2021-09-12 08:27:00 -0300

    Create Ellipse.java

---
+++ app/figures/Ellipse.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+
+public class Ellipse extends Figure2d {
+    public Ellipse (int x, int y, int w, int h, int r, int g, int b, int fr, int fg, int fb) {
+        super(x, y, w, h, r, g, b, fr, fg, fb);
+    }
+
+    public void print () {
+        System.out.format("Ellipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.fr, this.fg, this.fb));
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setColor(new Color(this.r, this.g, this.b));
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));    
+    }
+}
