64e0d83d311b96d1abb5929b50a4ed2e9864685d
LeoJosephson <leonardojosephson24@gmail.com>
2021-09-12 08:25:37 -0300

    Create Rect.java

---
+++ app/figures/Rect.java
@@ -0,0 +1,23 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure2d {
+
+    public Rect (int x, int y, int w, int h, int r, int g, int b, int fr, int fg, int fb) {
+        super(x, y, w, h, r, g, b, fr, fg, fb);
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.fr, this.fg, this.fb));
+        g2d.fillRect(this.x, this.y, this.w, this.h);
+        g2d.setColor(new Color(this.r, this.g, this.b));
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
